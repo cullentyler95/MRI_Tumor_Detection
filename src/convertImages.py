f81f34b480a6d2a8390acaca300e02f5739ae912
cullentyler95 <56904136+cullentyler95@users.noreply.github.com>
2024-05-06 21:36:27 -0400

    Delete src directory

--- src/convertImages.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import ants
-
-from antspynet.utilities import brain_extraction
-
-brain_mask =  brain_extraction('.\\data\\yes\\Y11.jpg', verbose=True)
